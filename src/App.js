3f6010b924496f25d1f6a19c299936175352e446
sagarpednekar <sagarpednekar04@gmail.com>
2018-07-11 00:23:30 +0530

    Set an example of state for subjects array to reverse and sort

--- src/App.js
+++ src/App.js
@@ -1,51 +1,83 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header';
-import Intro from './components/Intro'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Parent msg= "This msg is for parent " />
-       
+        <Marks  />
       </div>
     );
   }
 }
-// sample parent component
 
+/// Make default props
+class Marks extends Component {
+ //define default states here in constructor
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Sagar V Pednekar",
+      totalMarks: 550,
+      subjects: ["s-Maths", "s-English", "s-Marathi", "s-History", "s-Science"],
+      subjectWiseMarks: {
+        "Maths": 138,
+        "Marathi": 78,
+        "Hindi": 82,
+        "English": 51,
+        "History": 92,
+        "Science": 94
+      }
+    }
+  this.handleReverse = this.handleReverse.bind(this);
+  this.handleSort = this.handleSort.bind(this);
+  }
 
-class Parent extends Component {
+  handleReverse(){
+    this.setState( {subjects: this.state.subjects.reverse()} );
+  }
+  handleSort(){
+    this.setState({
+      subjects: this.state.subjects.sort()
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>Msg: {this.props.msg}</h1>
-        <Child msg = "Message for Child component"/>
-        <p> {this.props.names.map((name, i) =>{
-            return " "+ name
-        })}</p>
-      </div>
-    )
-  }
-}
-Parent.defaultProps = {
-  names: [
-    "Sagar",
-    "Shweta",
-    "Shraddha"
-  ]
-}
-class Child extends Component{
-  render(){
-    return(
-      <div>
-        <h1>{this.props.msg}</h1>
+        <button onClick={this.handleSort}>Sort</button>
+        <button onClick={this.handleReverse}>Reverse</button>
+        <h1 >Your Marks details: </h1>
+        <ul>
+        <Subject subjects={this.state.subjects} />
+          <li>{this.state.name}</li>
+          <li>Total Marks : {this.state.totalMarks}</li>  
+          <li>Subject Wise Marks:
+            <ul>
+              <li>Maths : {this.state.subjectWiseMarks.Maths}</li>
+              <li>English: {this.state.subjectWiseMarks.English}</li>
+              <li>Marathi: {this.state.subjectWiseMarks.Marathi}</li>
+              <li>History:  {this.state.subjectWiseMarks.History}</li>
+              <li>Science: {this.state.subjectWiseMarks.Science}</li>
+            </ul>
+          </li>
+        </ul>
       </div>
     )
   }
 }
 
+class Subject extends Component {
+  render() {
+    return (
+      <div>
+         <li >Courses: {this.props.subjects.map((subject,i)=>{
+           return <p>{subject}</p>
+         })}</li>
+      </div>
+    )
+  }
+}
 
 
 export default App;
