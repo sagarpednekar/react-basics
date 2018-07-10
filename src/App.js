f7acf07fa887bf1c66b021e11f72af50c64b7d4c
sagarpednekar <sagarpednekar04@gmail.com>
2018-07-10 22:33:25 +0530

    React prop types  with the example of Marks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import PropTypes from 'prop-types';
 import Header from './components/Header';
 import Intro from './components/Intro'
 
@@ -22,20 +23,59 @@ class Parent extends Component {
       <div>
         <h1>Msg: {this.props.msg}</h1>
         <Child msg = "Message for Child component"/>
-        <p> {this.props.names.map((name, i) =>{
-            return " "+ name
-        })}</p>
+        <Marks />
       </div>
     )
   }
 }
-Parent.defaultProps = {
-  names: [
-    "Sagar",
-    "Shweta",
-    "Shraddha"
-  ]
+
+/// Make default props
+class Marks extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Your Marks details: </h1>
+        <ul>
+          <li>{this.props.name}</li>
+          <li>Total Marks : {this.props.totalMarks}</li>
+          <li>Courses: {this.props.subjects}</li>
+          <li>Subject Wise Marks:
+            <ul>
+              <li>Maths : { this.props.subjectWiseMarks.Maths}</li>
+              <li>English: { this.props.subjectWiseMarks.English}</li>
+              <li>Marathi: { this.props.subjectWiseMarks.Marathi}</li>
+              <li>History:  { this.props.subjectWiseMarks.History}</li>
+              <li>Science: { this.props.subjectWiseMarks.Science}</li>
+            </ul>
+          </li>
+          </ul>
+      </div>
+    )
+  }
 }
+
+Marks.propTypes  = {
+  name : PropTypes.string,
+  totalMarks: PropTypes.number,
+  subjects: PropTypes.array,
+  subjectWiseMarks: PropTypes.object
+}
+
+Marks.defaultProps = {
+  name : "Sagar V Pednekar",
+  totalMarks: 550,
+  subjects: ["Maths","English", "Marathi", "History","Science"],
+  subjectWiseMarks: {
+    "Maths": 138,
+    "Marathi": 78,
+    "Hindi": 82,
+    "English": 51,
+    "History": 92,
+    "Science": 94
+  }
+
+}
+
 class Child extends Component{
   render(){
     return(
