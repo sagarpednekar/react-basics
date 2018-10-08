31c81e4dd726927cb2cadd201299b6de8eb9a65d
Mitesh Bhanushali <mitesh.bhanushali@accionlabs.com>
2018-10-08 17:07:10 +0530

    unnecessary code removed

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,20 +2,16 @@ import React, { Component } from 'react';
 class Intro extends Component {
     constructor(props){
         super(props);
-        this.name = "Sagar";
-        // default values
         this.state = {
-
+            name = "Sagar"
         }
     }
-    hello(name){
-        return "Hey "+ name;
-    }
     render() {
+        const {name} = this.state;
         const intro = "Introduction to react-ui";
         return (
         <div className="App-intro" >
-         <h1>Hello there this is {this.name} </h1>
+         <h1>Hello there this is {name} </h1>
         </div>
       )
     }
